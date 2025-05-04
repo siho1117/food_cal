12b890bd213769264e5e77b5c6ffb9ddb69b4a19
Your Name <siho1117@yahoo.com.hk>
2025-05-04 13:04:05 +0800

    Your commit message here

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -10,23 +10,28 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }) : super(key: key);
 
   @override
-  Size get preferredSize => const Size.fromHeight(60);
+  // Increase the height from 60 to 80
+  Size get preferredSize => const Size.fromHeight(70);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppTheme.secondaryBeige, // Fixed background color
+      color: AppTheme.secondaryBeige,
+      // Add some extra top padding to make sure the content isn't too close to the status bar
       child: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          // Increase the vertical padding from 8.0 to 16.0
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // Center the row items vertically to use the increased height
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               // App name - always shown, larger font size
               const Text(
-                'FOOD CAL',
+                'FOOD LLM',
                 style: TextStyle(
-                  fontSize: 32, // Larger font size
+                  fontSize: 32,
                   fontWeight: FontWeight.bold,
                   color: AppTheme.primaryBlue,
                   letterSpacing: 2,
