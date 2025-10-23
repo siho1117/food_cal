c678fc3f87986cbaa6d570e2cb35902e337e13ee
Your Name <siho1117@yahoo.com.hk>
2025-10-23 18:18:17 +0800

    Your commit message here

--- lib/l10n/generated/app_localizations_zh.dart
+++ lib/l10n/generated/app_localizations_zh.dart
@@ -517,58 +517,7 @@ class AppLocalizationsZhTw extends AppLocalizationsZh {
   AppLocalizationsZhTw(): super('zh_TW');
 
   @override
-  String get appTitle => 'FOOD LLM';
-
-  @override
-  String get homeTitle => '首頁';
-
-  @override
-  String get progressTitle => '進度';
-
-  @override
-  String get exerciseTitle => '運動';
-
-  @override
   String get settingsTitle => '設定';
-
-  @override
-  String get summaryTitle => '總結';
-
-  @override
-  String get cameraTitle => '食物辨識';
-
-  @override
-  String get breakfast => '早餐';
-
-  @override
-  String get lunch => '午餐';
-
-  @override
-  String get dinner => '晚餐';
-
-  @override
-  String get snack => '點心';
-
-  @override
-  String get calories => '卡路里';
-
-  @override
-  String get protein => '蛋白質';
-
-  @override
-  String get carbs => '碳水化合物';
-
-  @override
-  String get fat => '脂肪';
-
-  @override
-  String get cost => '費用';
-
-  @override
-  String get weight => '體重';
-
-  @override
-  String get height => '身高';
 
   @override
   String get save => '儲存';
@@ -580,46 +529,7 @@ class AppLocalizationsZhTw extends AppLocalizationsZh {
   String get retry => '重試';
 
   @override
-  String get loading => '載入中...';
-
-  @override
-  String get saving => '儲存中...';
-
-  @override
-  String get delete => '刪除';
-
-  @override
-  String get edit => '編輯';
-
-  @override
-  String get add => '新增';
-
-  @override
-  String get done => '完成';
-
-  @override
-  String get next => '下一步';
-
-  @override
-  String get back => '返回';
-
-  @override
   String get close => '關閉';
-
-  @override
-  String get camera => '相機';
-
-  @override
-  String get gallery => '相簿';
-
-  @override
-  String get analyzeFood => '分析食物';
-
-  @override
-  String get retake => '重拍';
-
-  @override
-  String get foodPhoto => '食物照片';
 
   @override
   String get language => '語言';
@@ -628,100 +538,16 @@ class AppLocalizationsZhTw extends AppLocalizationsZh {
   String get selectLanguage => '選擇語言';
 
   @override
-  String get profile => '個人檔案';
-
-  @override
   String get preferences => '偏好設定';
-
-  @override
-  String get about => '關於';
-
-  @override
-  String get help => '說明';
-
-  @override
-  String get feedback => '意見回饋';
 
   @override
   String get errorLoadingData => '資料載入錯誤';
 
   @override
-  String get tryAgain => '重試';
-
-  @override
   String get unknownError => '發生未知錯誤';
 
   @override
-  String get noInternetConnection => '無網路連線。請檢查網路後重試。';
-
-  @override
-  String get networkError => '網路錯誤。請重試。';
-
-  @override
-  String get serverError => '伺服器錯誤。請稍後重試。';
-
-  @override
-  String get foodAddedSuccess => '食物已新增至記錄中！';
-
-  @override
-  String get exerciseLoggedSuccess => '運動記錄成功！';
-
-  @override
-  String get profileUpdatedSuccess => '個人檔案更新成功！';
-
-  @override
   String get settingsSavedSuccess => '設定儲存成功！';
-
-  @override
-  String get today => '今天';
-
-  @override
-  String get yesterday => '昨天';
-
-  @override
-  String get thisWeek => '本週';
-
-  @override
-  String get thisMonth => '本月';
-
-  @override
-  String get daily => '每日';
-
-  @override
-  String get weekly => '每週';
-
-  @override
-  String get monthly => '每月';
-
-  @override
-  String get goal => '目標';
-
-  @override
-  String get target => '目標';
-
-  @override
-  String get remaining => '剩餘';
-
-  @override
-  String get consumed => '已消耗';
-
-  @override
-  String get burned => '已燃燒';
-
-  @override
-  String get progress => '進度';
-
-  @override
-  String get name => '姓名';
-
-  @override
-  String get age => '年齡';
-
-  @override
-  String get gender => '性別';
-
-  @override
-  String get activityLevel => '活動水平';
 
   @override
   String get units => '單位';
@@ -731,30 +557,6 @@ class AppLocalizationsZhTw extends AppLocalizationsZh {
 
   @override
   String get imperial => '英制';
-
-  @override
-  String get male => '男性';
-
-  @override
-  String get female => '女性';
-
-  @override
-  String get other => '其他';
-
-  @override
-  String get sedentary => '久坐';
-
-  @override
-  String get lightlyActive => '輕度活躍';
-
-  @override
-  String get moderatelyActive => '中度活躍';
-
-  @override
-  String get veryActive => '高度活躍';
-
-  @override
-  String get extremelyActive => '極度活躍';
 
   @override
   String get caloriesToday => '今日卡路里';
